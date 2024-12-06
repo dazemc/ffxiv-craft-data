4782b05b9669fa191a91bde7219c3035f15f9816
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-12-06 06:48:09 -0800

    add translations

--- main.py
+++ main.py
@@ -3,18 +3,43 @@ import re
 import json
 
 df = pd.read_csv('./recipe/Recipe.csv')
-df_level_table = pd.read_csv('./recipe/RecipeLevelTable.csv')
+df_de = pd.read_csv('./recipe/RecipeDE.csv')
+df_fr = pd.read_csv('./recipe/RecipeFR.csv')
+df_ja = pd.read_csv('./recipe/RecipeJA.csv')
 
+df_level_table = pd.read_csv('./recipe/RecipeLevelTable.csv')
 df_level_table.columns = df_level_table.iloc[0]
 df_level_table = df_level_table[2:]
 
 df.columns = df.iloc[0]
 df = df[1:]
-craft_types = list(df["CraftType"].unique())
-craft_types.pop(0)
+df_de.columns = df_de.iloc[0]
+df_de = df_de[1:]
+df_fr.columns = df_fr.iloc[0]
+df_fr = df_fr[1:]
+df_ja.columns = df_ja.iloc[0]
+df_ja = df_ja[1:]
+
+
+craft_types = list(df["CraftType"].unique())[1:]
+craft_types_de = list(df_de["CraftType"].unique())[1:]
+craft_types_fr = list(df_fr["CraftType"].unique())[1:]
+craft_types_ja = list(df_ja["CraftType"].unique())[1:]
+
+
 recipes_df = {}
+recipes_df_de = {}
+recipes_df_fr = {}
+recipes_df_ja = {}
 for craft in craft_types:
     recipes_df[craft] = df[df["CraftType"] == craft]
+for craft in craft_types_de:
+    recipes_df_de[craft] = df_de[df_de["CraftType"] == craft]
+for craft in craft_types_fr:
+    recipes_df_fr[craft] = df_fr[df_fr["CraftType"] == craft]
+for craft in craft_types_ja:
+    recipes_df_ja[craft] = df_ja[df_ja["CraftType"] == craft]
+
 
 export_recipes = {
     "Carpenter": [],
@@ -27,7 +52,8 @@ export_recipes = {
     "Alchemist": [],
 }
 
-for craft_type in recipes_df:
+
+for j, craft_type in enumerate(recipes_df):
     for i in range(len(recipes_df[craft_type])):
         match craft_type:
             case "Alchemy":
@@ -48,6 +74,9 @@ for craft_type in recipes_df:
                 craft_key = "Leatherworker"
         current_recipe = recipes_df[craft_type].iloc[i]
         name = current_recipe["Item{Result}"]
+        name_de = recipes_df_de[craft_types_de[j]].iloc[i]["Item{Result}"]
+        name_fr = recipes_df_fr[craft_types_fr[j]].iloc[i]["Item{Result}"]
+        name_ja = recipes_df_ja[craft_types_ja[j]].iloc[i]["Item{Result}"]
         level_table_num = [num for num in re.findall(r'\d+', current_recipe["RecipeLevelTable"])][0]
         current_recipe["RecipeLevelTable"] = df_level_table.iloc[int(level_table_num)]  # append level table
         current_level_table = current_recipe["RecipeLevelTable"]
@@ -70,7 +99,10 @@ for craft_type in recipes_df:
                 "level": level,
                 "maxQuality": quality,
                 "name": {
+                    "de": name_de,
                     "en": name,
+                    "fr": name_fr,
+                    "ja": name_ja,
                 },
                 "progressDivider": progress_divider,
                 "progressModifier": progress_modifier,
@@ -83,6 +115,6 @@ for craft_type in recipes_df:
 
 
 for crafter in export_recipes:
-    with open(f"./export/{crafter}.json", "w") as json_file:
+    with open(f"./export/{crafter}.json", "w", encoding="utf-8") as json_file:
         json.dump(export_recipes[crafter], json_file, indent=2, sort_keys=True, ensure_ascii=False)
 
