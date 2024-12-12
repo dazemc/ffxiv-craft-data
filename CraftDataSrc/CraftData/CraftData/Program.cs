fa32e0a2d9d7fd616b0d9a1d7e0007d58bfdb938
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-12-12 04:46:25 -0800

    RecipeLevelTable

--- CraftDataSrc/CraftData/CraftData/Program.cs
+++ CraftDataSrc/CraftData/CraftData/Program.cs
@@ -36,12 +36,14 @@ class Program
 
             // Retrieve the Crafting Recipe sheet
             var recipes = realm.GameData.GetSheet<SaintCoinach.Xiv.Recipe>();
+            var recipeLevelTable = realm.GameData.GetSheet<SaintCoinach.Xiv.RecipeLevelTable>();
 
             // Create the CSV file path
-            var csvFilePath = @"./export/Recipe.csv";
+            var csvRecipe = @"./export/Recipe.csv";
+            var csvRecipeLevelTable = @"./export/RecipeLevelTable.csv";
 
             // Open or create the CSV file
-            using (var writer = new StreamWriter(csvFilePath))
+            using (var writer = new StreamWriter(csvRecipe))
             {
                 // Write the header row to the CSV file
                 writer.WriteLine("#,CraftType,RecipeLevelTable,Name,MaterialQualityFactor,DifficultyFactor,QualityFactor,DurabilityFactor,RequiredControl,RequiredCraftsmanship");
@@ -51,6 +53,14 @@ class Program
                 {
                     // Write recipe data to CSV file (you can add more columns if needed)
                     writer.WriteLine($"{recipe.Key},{recipe.CraftType},{recipe.RecipeLevelTable},{recipe},{recipe.MaterialQualityFactor},{recipe.DifficultyFactor},{recipe.QualityFactor},{recipe.DurabilityFactor},{recipe.RequiredControl},{recipe.RequiredCraftsmanship}");
+                }
+            }
+            using (var levelWriter = new StreamWriter(csvRecipeLevelTable))
+            {
+                levelWriter.WriteLine("#", "Stars", "Difficulty");
+                foreach (var level in recipeLevelTable) 
+                {
+                    levelWriter.WriteLine($"{level.Key},{level.Stars}.{level.Difficulty}");
                 }
             }
 
