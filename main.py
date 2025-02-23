bb106b9191b042d3c76e55b890e772093b7aa28a
Daazed McFarland <daazedjmcfarland@gmail.com>
2025-02-22 23:57:30 -0800

    merge by key, set missing values to default to english

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import asyncio
+
 import json
 import mmap
 import os
 import struct
+
 from io import StringIO
 
 import pandas as pd
 
+
 FFXIV_FILEPATH: str = "G:/SteamLibrary/steamapps/common/FINAL FANTASY XIV Online"
+IMPORT_RECIPE_DIR_KO: str = "./import/ko/recipedb"
+IMPORT_BUFFS_DIR_KO: str = "./import/ko/buffs"
+
 
 CRAFTDATA_FILEPATH: str = "/CraftData/Release/net7.0/win-x64/publish/"
+
 CURRENT_FILEPATH: str = os.path.abspath(__file__).replace("main.py", "")
+
 
 CRAFTDATA_CONFIG: dict = {
     "GameDirectory": FFXIV_FILEPATH,
@@ -35,10 +43,15 @@ async def run_exe_waiting_input() -> asyncio.subprocess.Process:
 
 def read_shared_memory() -> tuple:  # <StringIO>
     shared_memory_name: str = "Global\\CraftData"
+
     memory_size: int = 0
+
     memory_offset: int = 8
+
     memory_size += memory_offset
+
     buffer_size = 0
+
     next_buffer_size = 0
 
     try:
@@ -46,8 +59,11 @@ def read_shared_memory() -> tuple:  # <StringIO>
             -1, memory_size, tagname=shared_memory_name, access=mmap.ACCESS_READ
         ) as mm:
             buffer_size = struct.unpack_from("i", mm, 0)[0]
+
             next_buffer_size = struct.unpack_from("i", mm, 4)[0]
+
             memory_size += buffer_size + next_buffer_size
+
         with mmap.mmap(-1, memory_size, tagname=shared_memory_name) as mm:
             data: str = (
                 mm[memory_offset:buffer_size].decode("utf-8").replace("\x00", "")
@@ -55,9 +71,13 @@ def read_shared_memory() -> tuple:  # <StringIO>
             data_item: str = (
                 mm[buffer_size:memory_size].decode("utf-8").replace("\x00", "")
             )
+
             csv_item_buffer: StringIO = StringIO(data_item)
+
             csv_buffer: StringIO = StringIO(data)
+
             return csv_buffer, csv_item_buffer
+
     except Exception as e:
         print(f"Error: {e}")
 
@@ -77,28 +97,38 @@ async def terminate_signal(process) -> bool:
     async for line in process.stdout:
         if "terminate" in line.decode("utf-8").strip():
             return False
+
         return True
 
 
 async def create_csv() -> StringIO:
     process_task: asyncio.Task = asyncio.create_task(run_exe_waiting_input())
+
     process: asyncio.Task = await process_task
+
     while await terminate_signal(process):
         await asyncio.sleep(0.1)
+
     csv_data: tuple = read_shared_memory()
+
     csv: StringIO = csv_data[0]
+
     csv_item: StringIO = csv_data[1]
     if process:
         process.terminate()
+
     await process.wait()
+
     return csv, csv_item
 
 
 def read_csv(csv: StringIO) -> pd.DataFrame:
     try:
         df: pd.DataFrame = pd.read_csv(csv)
+
         df.dropna(inplace=True)
         return df
+
     except Exception as e:
         print(f"Error reading csv: {e}")
 
@@ -112,9 +142,44 @@ def separate_crafters(crafts: list, df: pd.DataFrame) -> dict:
 
 def separate_buffs(buffs: list, df: pd.DataFrame) -> dict:
     buffs_df: dict = {}
+
     for buff in buffs:
         buffs_df[buff] = df[df.Category == buff]
+
     return buffs_df
+
+
+def merge_json(primary: dict, secondary: dict):
+    secondary_lookup = {
+        recipe["key"]: recipe["name"].get("ko")
+        for category in secondary.values()
+        for recipe in category
+        if "name" in recipe and "ko" in recipe["name"]
+    }
+
+    for category, recipes in primary.items():
+        for recipe in recipes:
+            key = recipe.get("key")
+            if key in secondary_lookup:
+                recipe["name"]["ko"] = secondary_lookup[key]
+
+
+def is_import_available() -> bool:
+    return os.path.isdir(IMPORT_RECIPE_DIR_KO)
+
+
+def get_ko_dict() -> dict:
+    ko_json = {}
+
+    json_files_list = os.listdir(IMPORT_RECIPE_DIR_KO)
+
+    for crafter_file in json_files_list:
+        with open(
+            f"{IMPORT_RECIPE_DIR_KO}/{crafter_file}", "r", encoding="utf-8"
+        ) as json_recipe:
+            ko_json[crafter_file[:-5]] = json.load(json_recipe)
+
+    return ko_json
 
 
 def create_export_recipe(recipes_df: dict) -> dict:
@@ -128,69 +193,95 @@ def create_export_recipe(recipes_df: dict) -> dict:
         "Culinarian": [],
         "Alchemist": [],
     }
-    ko_dir = "./import/ko/recipedb/"
-    ko_json = {}
-    if os.path.isdir(ko_dir):
-        json_files_list = os.listdir(ko_dir)
-        for crafter_file in json_files_list:
-            with open(
-                f"./import/ko/recipedb/{crafter_file}", "r", encoding="utf-8"
-            ) as json_recipe:
-                ko_json[crafter_file[:-5]] = json.load(json_recipe)
+
     for craft_type in recipes_df:
         for i in range(len(recipes_df[craft_type])):
             match craft_type:
                 case "Alchemy" | "연금":
                     craft_key: str = "Alchemist"
+
                 case "Armorcraft" | "갑주제작":
                     craft_key: str = "Armorer"
+
                 case "Smithing" | "대장일":
                     craft_key: str = "Blacksmith"
+
                 case "Woodworking" | "목공":
                     craft_key: str = "Carpenter"
+
                 case "Cooking" | "요리":
                     craft_key: str = "Culinarian"
+
                 case "Goldsmithing" | "보석공예":
                     craft_key: str = "Goldsmith"
+
                 case "Clothcraft" | "재봉":
                     craft_key: str = "Weaver"
+
                 case "Leatherworking" | "가죽공예":
                     craft_key: str = "Leatherworker"
+
             current_recipe: dict = recipes_df[craft_type].iloc[i]
+
+            recipe_key: int = int(current_recipe.Key)
+
             name: str = current_recipe.get("Name", "") or ""
-            name_de: str = str(current_recipe.get("NameDE", "") or "").replace(
+
+            name_de: str = str(current_recipe.get("NameDE", "") or name).replace(
                 "<SoftHyphen/>", "\u00ad"
             )
-            name_fr: str = str(current_recipe.get("NameFR", "") or "").replace(
+
+            name_fr: str = str(current_recipe.get("NameFR", "") or name).replace(
                 "<SoftHyphen/>", "\u00ad"
             )
-            name_ja: str = current_recipe.get("NameJA", "") or ""
-            if i < len(ko_json[craft_key]):
-                name_ko: str = ko_json[craft_key][i]["name"]["ko"]
-            else:
-                print(name) # Check for any missing results
+
+            name_ja: str = current_recipe.get("NameJA", "") or name
+
+            name_ko: str = current_recipe.get("NameKO", "") or name
+
             difficulty: int = int(current_recipe.Difficulty)
+
             durability: int = int(current_recipe.Durability)
+
             base_level: int = int(current_recipe.ClassJobLevel)
+
             level: int = int(current_recipe.Level)
+
             quality: int = int(current_recipe.Quality)
+
             difficulty_factor: int = int(current_recipe.DifficultyFactor) / 100
+
             quality_factor: int = int(current_recipe.QualityFactor) / 100
+
             durability_factor: int = int(current_recipe.DurabilityFactor) / 100
+
             difficulty *= difficulty_factor
+
             durability *= durability_factor
+
             quality *= quality_factor
+
             difficulty: int = int(difficulty // 1)
+
             durability: int = int(durability // 1)
+
             quality: int = int(quality // 1)
+
             progress_divider: int = int(current_recipe.ProgressDivider)
+
             progress_modifier: int = int(current_recipe.ProgressModifier)
+
             quality_divider: int = int(current_recipe.QualityDivider)
+
             quality_modifier: int = int(current_recipe.QualityModifier)
+
             suggested_craft: int = int(current_recipe.SuggestedCraftsmanship)
+
             stars: int = int(current_recipe.Stars)
+
             export_recipes[craft_key].append(
                 {
+                    "key": recipe_key,
                     "baseLevel": base_level,
                     "difficulty": difficulty,
                     "durability": durability,
@@ -211,61 +302,82 @@ def create_export_recipe(recipes_df: dict) -> dict:
                     "stars": stars,
                 }
             )
+
     return export_recipes
 
 
-def create_export_buffs(df):
+def create_export_buffs(df) -> dict:
     export_buffs = {"Meal": [], "Medicine": []}
+
     buff_key = ""
+
     for buff in df:
         for i in range(len(df[buff])):
             buff_key = buff
+
             if buff == "Meals":
                 buff_key = "Meal"
+
             current_item = df[buff].iloc[i]
+
             buff_types = {}
+
             buff_types_hq = {}
+
             for j in range(3):
                 match current_item[f"BuffType{j + 1}"]:
                     case "CP":
                         buff_types["cp_percent"] = int(
                             current_item[f"PercentageValue{j + 1}"]
                         )
+
                         buff_types["cp_value"] = int(current_item[f"MaxValue{j + 1}"])
+
                         buff_types_hq["cp_percent"] = int(
                             current_item[f"PercentageValueHQ{j + 1}"]
                         )
+
                         buff_types_hq["cp_value"] = int(
                             current_item[f"MaxValueHQ{j + 1}"]
                         )
+
                     case "Craftsmanship":
                         buff_types["craftsmanship_percent"] = int(
                             current_item[f"PercentageValue{j + 1}"]
                         )
+
                         buff_types["craftsmanship_value"] = int(
                             current_item[f"MaxValue{j + 1}"]
                         )
+
                         buff_types_hq["craftsmanship_percent"] = int(
                             current_item[f"PercentageValueHQ{j + 1}"]
                         )
+
                         buff_types_hq["craftsmanship_value"] = int(
                             current_item[f"MaxValueHQ{j + 1}"]
                         )
+
                     case "Control":
                         buff_types["control_percent"] = int(
                             current_item[f"PercentageValue{j + 1}"]
                         )
+
                         buff_types["control_value"] = int(
                             current_item[f"MaxValue{j + 1}"]
                         )
+
                         buff_types_hq["control_percent"] = int(
                             current_item[f"PercentageValueHQ{j + 1}"]
                         )
+
                         buff_types_hq["control_value"] = int(
                             current_item[f"MaxValueHQ{j + 1}"]
                         )
+
                     case "Nothing":
                         break
+
             for k in range(2):
                 info = {
                     "hq": False if k == 0 else True,
@@ -287,33 +399,44 @@ def create_export_buffs(df):
 
                 if k == 0:
                     buff_types.update(info)
+
                     export_buffs[buff_key].append(buff_types)
                 else:
                     buff_types_hq.update(info)
+
                     export_buffs[buff_key].append(buff_types_hq)
+
     return export_buffs
 
 
 def export(export_recipes: dict, export_buffs: dict) -> None:
     path: str = "../../data/recipedb/"
+
     path_item: str = "../../data/buffs/"
+
     is_ko = export_recipes["Weaver"][0]["name"].get("ko", False)
+
     is_en = export_recipes["Weaver"][0]["name"].get("en", False)
 
     if is_ko and not is_en:
-        path: str = "./import/ko/recipedb"
-        path_item: str = "./import/ko/buffs"
+        path: str = f"{IMPORT_RECIPE_DIR_KO}"
+
+        path_item: str = f"{IMPORT_BUFFS_DIR_KO}"
 
     elif not os.path.isdir(path):
         path: str = "./export/recipedb/"
+
         path_item: str = "./export/buffs/"
 
     else:
         path: str = "../../data/recipedb/"
+
         path_item: str = "../../data/buffs/"
 
     os.makedirs(path, exist_ok=True)
+
     os.makedirs(path_item, exist_ok=True)
+
     for crafter in export_recipes:
         with open(f"./{path}/{crafter}.json", "w", encoding="utf-8") as json_file:
             json.dump(
@@ -323,6 +446,7 @@ def export(export_recipes: dict, export_buffs: dict) -> None:
                 sort_keys=True,
                 ensure_ascii=False,
             )
+
     for buff in export_buffs:
         with open(f"./{path_item}/{buff}.json", "w", encoding="utf-8") as json_file:
             json.dump(
@@ -349,6 +473,9 @@ async def main() -> None:
     ordered_recipe: dict = separate_crafters(crafts=craft_types, df=df_recipe)
     export_buffs: dict = create_export_buffs(df=ordered_buff)
     export_recipes: dict = create_export_recipe(recipes_df=ordered_recipe)
+    if is_import_available():
+        ko_recipes = get_ko_dict()
+        ordered_recipe = merge_json(export_recipes, ko_recipes)
     export(export_recipes=export_recipes, export_buffs=export_buffs)
 
 
