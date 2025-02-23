3f1d361af31bbcb5b377e75fdc4789eb52a432d2
Daazed McFarland <daazedjmcfarland@gmail.com>
2025-02-23 01:04:22 -0800

    clean up

--- CraftDataSrc/CraftData/CraftData/obj/Debug/net7.0/CraftData.AssemblyInfo.cs
+++ CraftDataSrc/CraftData/CraftData/obj/Debug/net7.0/CraftData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CraftData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8479993cb788475a4d355cf6e6b03de7b9fe14d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95bb8a07ba612b89848ff158688b29d94f2ccee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CraftData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CraftData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
