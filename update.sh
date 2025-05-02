7182a27278056496f48c807e699cc96755648c5e
Daazed McFarland <daazedjmcfarland@gmail.com>
2025-05-02 04:19:44 -0700

    add build in update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 git submodule update --init --recursive
 git submodule foreach git pull origin master
+cd ./src/SaintcoinachSrc/
+dotnet build -c Release
+cd ../../src/CraftDataSrc/CraftData/
+dotnet publish -c Release
+cd ../../
 git add .
-git commit -m "updated submodules"
+git commit -m "updated submodules and build"
 git push
 exit
